b0f4628f2048312b54d71344facaa0625798ca56
sondev <sonvvde180764@fpt.edu.vn>
2025-07-06 09:09:46 +0700

    carasel vs modal

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,6 +1,19 @@
 import PropTypes from "prop-types"
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import Modal from 'react-modal';
+import YouTube from "react-youtube";
+import { useState } from "react"
+
+const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+    },
+};
+
 const responsive = {
     superLargeDesktop: {
         // the naming can be any, depends on you.
@@ -21,13 +34,38 @@ const responsive = {
     }
 };
 const MovieList = ({ title, data }) => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [trailerKey, setTrailerKey] = useState("");
+
+    const handleTrailer = async (id) => {
+        setTrailerKey('');
+        try {
+            const url = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`;
+            const options = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+                }
+            };
+            const movieKey = await fetch(url, options);
+            const data = await movieKey.json();
+            setTrailerKey(data.results[0].key);
+            setModalIsOpen(true);
+        } catch (error) {
+            setModalIsOpen(false);
+            console.log(error)
+        }
+    }
+
     return (
         <div className='text-white p-10 mb-10'>
             <h2 className='uppercase text-xl  mb-4'>{title}</h2>
             <Carousel responsive={responsive}
                 className='flex items-center space-x-4' >
                 {data.length > 0 && data.map((item) => (
-                    <div key={item.id} className='w-[200px] h-[300px] relative group'>
+                    <div key={item.id} className='w-[200px] h-[300px] relative group'
+                        onClick={() => handleTrailer(item.id)}>
                         <div className='group-hover:scale-105 cursor-pointer
                             transition-transform duration-500 ease-in-out w-full h-full'>
                             <div className='absolute top-0 left-0 w-full h-full bg-black/40'>
@@ -42,7 +80,28 @@ const MovieList = ({ title, data }) => {
                     </div>
                 ))}
             </Carousel>
-        </div>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={() => setModalIsOpen(false)}
+                style={{
+                    overlay: {
+                        position: "fixed",
+                        zIndex: 9999,
+                    },
+                    content: {
+                        top: "50%",
+                        left: "50%",
+                        right: "auto",
+                        bottom: "auto",
+                        marginRight: "-50%",
+                        transform: "translate(-50%,-50%)",
+                    },
+                }}
+                contentLabel="Example Modal"
+            >
+                <YouTube videoId={trailerKey} opts={opts} />;
+            </Modal>
+        </div >
     )
 }
 
